ea011a95e735a715a43b3da5477a986fb309c09a
Joao Teles <joaojoseteles@gmail.com>
2022-12-03 15:23:56 -0300

    :iphone: ajustando reponsividade do component slide

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -6,17 +6,44 @@ import SlideCard from "../slideCard";
 interface props {
   course: CourseType[];
 }
+
 export default function SlideComponent({ course }: props) {
+  let slideCount = 0;
+
+  if (course.length > 4) {
+    slideCount = 4;
+  } else if (course) {
+    slideCount = course.length;
+  }
   return (
     <>
       <div className="d-flex flex-column align-items-center py-4">
         <Splide
           options={{
             type: "loop",
-            perPage: 4,
+            perPage: slideCount,
             perMove: 1,
-            width: 1200,
+            width: slideCount * 300,
             pagination: false,
+            arrows: course.length > 4 ? true : false,
+            drag: course.length > 4 ? true : false,
+            breakpoints: {
+              1200: {
+                perPage: slideCount >= 2 ? 2 : 1,
+                width: slideCount >= 2 ? 640 : 300,
+                arrows: course.length > 2 ? true : false,
+                drag: course.length > 2 ? true : false,
+              },
+              640: {
+                width: 300,
+                perPage: 1,
+                arrows: course.length > 1 ? true : false,
+                drag: course.length > 1 ? true : false,
+              },
+              300: {
+                width: 250,
+              },
+            },
           }}
         >
           {course?.map((course) => (
