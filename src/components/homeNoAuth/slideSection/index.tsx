cb1297a0cdf69c9346b9e3e6cfc4a950df58d86f
Joao Teles <joaojoseteles@gmail.com>
2022-12-03 00:04:24 -0300

    :lipstick: feat: Estilizaçao CSS do component slide

--- src/components/homeNoAuth/slideSection/index.tsx
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -11,7 +11,7 @@ interface props {
 export default function ({ newestCourses }: props) {
   return (
     <>
-      <Container>
+      <Container className="d-flex flex-column align-items-center py-5">
         <p className={styles.sectionTitles}>AULAS JÁ DISPONÍVEIS</p>
         <SlideComponent course={newestCourses} />
         <Link href="/register">
