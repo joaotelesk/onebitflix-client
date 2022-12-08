9fb845dc20f2d5ba359e2ee139fb315225f93ff5
Joao Teles <joaojoseteles@gmail.com>
2022-12-08 18:16:11 -0300

    :recycle: refactor: utilizando as sections do component HomeAuth

--- pages/home.tsx
+++ pages/home.tsx
@@ -8,6 +8,11 @@ import { useRouter } from "next/router";
 
 import { useEffect, useState } from "react";
 import HeaderAuth from "../src/components/common/headerAuth";
+import FeaturedSection from "../src/components/homeAuth/featuredSection";
+import NewestCategory from "../src/components/homeAuth/newestCategory";
+import FavoriteCategory from "../src/components/homeAuth/favoriteCategory";
+import FeaturedCategory from "../src/components/homeAuth/featuredCategory";
+import ListCategories from "../src/components/homeAuth/listCategories";
 
 export default function HomeAuth() {
   return (
@@ -17,7 +22,12 @@ export default function HomeAuth() {
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <main>
-        <HeaderAuth />
+        <FeaturedSection />
+        <NewestCategory />
+        <FavoriteCategory />
+        <FeaturedCategory />
+        <ListCategories />
+        <Footer />
       </main>
     </>
   );
