f8548dc7766cb9455321784a1bea453921cbf112
Joao Teles <joaojoseteles@gmail.com>
2022-12-18 22:06:39 -0300

    :sparkles: feat: conexao com endpoint back-end e criaçao de metodos de leitura e modificaçao de dados do usuario

---
+++ src/services/profileService.ts
@@ -0,0 +1,45 @@
+import api from "./api";
+
+interface UserParams {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  createdAt: string;
+}
+
+const profileService = {
+  fetchCurrent: async () => {
+    const token = sessionStorage.getItem("onebitflix-token");
+    const res = await api
+      .get("/users/current", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((err) => {
+        return err.response;
+      });
+
+    return res.data;
+  },
+  userUpdate: async (params: UserParams) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+    const res = await api
+      .put("/users/current", params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((err) => {
+        if (err.response.status === 400 || err.response.status === 401) {
+          return err.response;
+        }
+        return err;
+      });
+
+    return res.status;
+  },
+};
+
+export default profileService;
