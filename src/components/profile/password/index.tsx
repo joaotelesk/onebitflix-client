466a71add4ba195cd9ca9ca007f31c7828978aa8
Joao Teles <joaojoseteles@gmail.com>
2022-12-19 14:30:20 -0300

    :sparkles: feat: componnet PasswordForm

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,1 +1,63 @@
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../../styles/profile.module.scss";
+
+export default function PasswordForm() {
+  return (
+    <>
+      <Form className={styles.form}>
+        <div className={styles.inputNormalDiv}>
+          <FormGroup>
+            <Label for="currentPassword" className={styles.label}>
+              SENHA ATUAL
+            </Label>
+            <Input
+              name="currentPassword"
+              id="currentPassword"
+              type="password"
+              placeholder="************"
+              required
+              minLength={6}
+              maxLength={12}
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+        </div>
+        <div className={styles.inputFlexDiv}>
+          <FormGroup>
+            <Label for="newPassword" className={styles.label}>
+              NOVA SENHA
+            </Label>
+            <Input
+              name="newPassword"
+              id="newPassword"
+              type="password"
+              placeholder="********"
+              required
+              minLength={6}
+              maxLength={12}
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="confirmNewPassword" className={styles.label}>
+              CONFIRMAR NOVA SENHA
+            </Label>
+            <Input
+              name="confirmNewPassword"
+              id="confirmNewPassword"
+              type="password"
+              placeholder="********"
+              required
+              minLength={6}
+              maxLength={12}
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+        </div>
+        <Button className={styles.formBtn} outline type="submit">
+          Salvar Alteração
+        </Button>
+      </Form>
+    </>
+  );
+}
