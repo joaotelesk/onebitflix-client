b6a98fa9d151f1e3f08decd5ce372b12b92c2822
Joao Teles <joaojoseteles@gmail.com>
2022-12-18 22:11:34 -0300

    :bulb: docs: exclusao de imports desnecessarios

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,13 +1,5 @@
-import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
-import HeaderGeneric from "../src/components/common/headerGeneric";
-
 import Footer from "../src/components/common/footer";
-import authService from "../src/services/authService";
-import { useRouter } from "next/router";
-
-import { useEffect, useState } from "react";
-import HeaderAuth from "../src/components/common/headerAuth";
 import FeaturedSection from "../src/components/homeAuth/featuredSection";
 import NewestCategory from "../src/components/homeAuth/newestCategory";
 import FavoriteCategory from "../src/components/homeAuth/favoriteCategory";
@@ -18,7 +10,7 @@ export default function HomeAuth() {
   return (
     <>
       <Head>
-        <title>Onebitflix - home</title>
+        <title>Onebitflix - Home</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <main>
