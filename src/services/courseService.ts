0ad5dabbff6afb55e58d61cd5f1128fc6edf9f98
Joao Teles <joaojoseteles@gmail.com>
2022-12-20 16:58:25 -0300

    :sparkles: feat: metodo e conexao com end-point de search no servidor

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -82,6 +82,20 @@ const courseService = {
       });
     return res;
   },
+  getSearch: async (name: string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/search?name=${name}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((err) => {
+        return err.response;
+      });
+    return res;
+  },
 };
 
 export default courseService;
